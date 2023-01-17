0e81833ed641b33a46c459abda7dd34cf89ee136
Juan Ignacio <jibocchi14@gmail.com>
2023-01-17 14:59:41 -0300

    Adding products

--- src/manager.js
+++ src/manager.js
@@ -1,11 +1,19 @@
 import ProductManager from "./app.js";
 
-let productManager = new ProductManager('./files/products.json');
+let productManager = new ProductManager('./src/files/products.json');
 
 const Products = async () => {
  //Empezar a utilizar el productManager 
-    await productManager.addProduct("Computadora", "La mejor compu", 120000, "No thumbail", 23, "compu")
-    await productManager.addProduct("Telefono", "El mejor telefono", 150000, "No thumbail", 2, "tel")
+    await productManager.addProduct("Celular", "El mejor celular", 150000, "No thumbail", 22, "cel")
+    await productManager.addProduct("Auto", "El mejor auto", 15300000, "No thumbail", 6, "auto")
+    await productManager.addProduct("Heladera", "La mejor heladera", 153000, "No thumbail", 20, "heladera")
+    await productManager.addProduct("Libro", "El mejor libro", 1000, "No thumbail", 12, "libro")
+    await productManager.addProduct("Ipod", "El mejor ipod", 1500, "No thumbail", 9, "ipod")
+    await productManager.addProduct("Iphone", "El mejor iphone", 350000, "No thumbail", 5, "iphone")
+    await productManager.addProduct("Auriculares", "El mejor auricular", 15000, "No thumbail", 1, "auris")
+    await productManager.addProduct("Teclado", "El mejor teclado", 10000, "No thumbail", 16, "teclado")
+    await productManager.addProduct("Mouse", "El mejor mouse", 8500, "No thumbail", 29, "mouse")
+    await productManager.addProduct("Silla gamer", "La mejor silla gamer", 100000, "No thumbail", 9, "silla")
 }
 
 Products();
